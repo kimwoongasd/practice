6222940a879a6f0db876d4f95b7fd5f4bc5f7cd7
woong <dndvud123@naver.com>
2022-03-03 17:23:51 +0900

    Unit class add function damaged

--- nadocoding.py
+++ nadocoding.py
@@ -7,3 +7,12 @@ class Unit:
         
     def move(self, location):
         print(f'[지상 유닛 이동] {self.namae} : {location} 방향으로 이동합니다. [속도 {self.speed}]')
+        
+    def damaged(self, damage):
+        print(f'{self.name} : {damage} 데미지를 입었습니다.')
+        self.hp -= damage
+        if self.hp > 0:
+            print(f'{self.name} : 현재 체력은 {self.hp}입니다.')
+        
+        else:
+            print(f'{self.name} 유닛이 파괴 되었습니다.')
