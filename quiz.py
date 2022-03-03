1cd7ee12868b6e4402d43d6dba59dab2c91d9267
woong <dndvud123@naver.com>
2022-03-03 21:24:21 +0900

    modify cod

--- quiz.py
+++ quiz.py
@@ -17,11 +17,14 @@ class House:
     def show_detail(self):
         print(f'{self.location} {self.house_type} {self.deal_type} {self.price} {self.completion_year}')
 
-
+houses = []
 house_1 = House('강남', '아파트', '매매', '10억', '2010년')
 house_2 = House('마포', '오피스텔', '매매', '5억', '2017년')
 house_3 = House('송파', '빌라', '월세', '500/50', '2020년')
 
-house_1.show_detail()
-house_2.show_detail()
-house_3.show_detail()
+houses.append(house_1)
+houses.append(house_2)
+houses.append(house_2)
+
+for house in houses:
+    house.show_detail()
