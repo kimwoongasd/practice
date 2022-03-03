613a5ce58c21610dd4596d89ff48b43df1a7aa3d
woong <dndvud123@naver.com>
2022-03-03 21:14:42 +0900

    quiz

---
+++ quiz.py
@@ -0,0 +1,13 @@
+# 주어진 코드를 활용하여 부동산 프로그램 작성
+# 출력 예제
+# 강남 아파트 매매 10억 2010년
+# 마포 오피스텔 매매 5억 2017년
+# 송파 빌라 월세 500/50 2020년
+
+class House:
+    def __init__(self, location, house_type, deal_type, price, completion_year):
+        pass
+    
+    def show_detail(self):
+        pass
+        
